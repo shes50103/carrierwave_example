17570637530737ccfdecde46f430b05536bf60bb
shes50103 <shes50103@gmail.com>
2020-05-19 10:32:57 +0800

    feat: 使用 CarrierWave::Uploader

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -69,6 +69,6 @@ class PeopleController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def person_params
-      params.require(:person).permit(:f_name, :l_name, :status, :country)
+      params.require(:person).permit(:f_name, :l_name, :status, :country, :avatar)
     end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+
 end
